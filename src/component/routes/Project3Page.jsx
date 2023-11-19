727b48f9f71244f0887b6908ee191b9fe7fe5a8d
lawrenzoo <chiagozielawrence@gmail.com>
2023-11-19 01:07:05 +0100

    firth commit

--- src/component/routes/Project3Page.jsx
+++ src/component/routes/Project3Page.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import NavBar from '../NavBar/NavBar'
-import Shopmart from '../shopmart/Shopmart'
+// import React from 'react'
+// // import NavBar from '../NavBar/NavBar'
+// // import Shopmart from '../shopmart/Shopmart'
 
-const Project3Page = () => {
-  return (
-    <>
-    <NavBar/>
-    <Shopmart/>
-    </>
-  )
-}
+// const Project3Page = () => {
+//   return (
+//     <>
+//     </>
+//   )
+// }
 
-export default Project3Page
+// export default Project3Page
